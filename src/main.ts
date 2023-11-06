bf205f7260d19219b6c239ce7f160f173e0e2e48
Nela Bojdova <nela.bojdova@gmail.com>
2023-11-06 20:54:26 +0100

    zone and dependency for stackblitz

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import routeConfig from './app/app-routing.module';
+import 'zone.js/dist/zone';
 
 import { AppComponent } from './app/app.component';
 
